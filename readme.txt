754d15b6814896a05510a2a62bc09f41630494de
fititnt <emerson@webdesign.eng.br>
2011-09-12 13:23:24 -0300

    Created. Forked from joomleiros template from #jbr11

---
+++ readme.txt
@@ -0,0 +1,18 @@
+PHP | Joomla | Template | Joomla 1.6+ Template helper
+
+Just a base test template help what will me, initial author, will be asked to help with joomleiros template, or any
+other community project that I get envolved.
+
+
+-------------------------------- Changelog --------------------------------
+-2011-09-12: Initial release
+! Created. Forked from joomleiros template from #jdbr11
+
+
+CHANGELOG LEGEND:
++ Added
+- Removed
+^ Updated
+* Bugfix
+# Security Fix
+! Relevant message
---
+++ tpl_jth/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <jdoc:include type="head" />
+    <link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/joomleiros/css/joomleiros.css" type="text/css" />
+</head>
+
+<body>
+  <div id="all">
+   <header class="row">
+       <div class="column grid_12">
+        <jdoc:include type="modules" name="topo" style="xhtml"/>
+        </div>
+        <nav class="row">
+    <div class="column grid_12">   
+        <jdoc:include type="modules" name="menu" style="xhtml"/>
+    </div>
+   </nav>
+   </header>.
+   <div class="row">
+    <div class="column grid_12">
+    <jdoc:include type="message" />
+    </div>
+   </div>
+   <aside class="row">
+       <div class="column grid_4">
+            <jdoc:include type="modules" name="coluna1" style="xhtml"/>
+        </div>
+   </aside>
+   <div id="row conteudo">
+        <div class="column grid_8">
+             <jdoc:include type="component" />
+     </div>
+   <aside class="row">
+        <div class="column grid_4">
+            <jdoc:include type="modules" name="coluna2" style="xhtml"/>
+        </div>
+   </aside>
+   <footer class="row">
+        <div class="column grid_12">
+            <jdoc:include type="modules" name="rodape" style="xhtml"/>
+        </div>
+   </footer>
+    <script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/joomleiros/js/joomleiros.js"></script>
+   </footer>
+  </div>
+</body>
+</html>
